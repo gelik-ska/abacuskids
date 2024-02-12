08fc4e0f7a5c91c8c245fa881c163308f6fafb35
Tatyana <t.chebun@gmail.com>
2024-02-12 22:53:08 +0200

    fix mail

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
 !empty($_POST['EMAIL']) ? $email=strip_tags($_POST['EMAIL']):$result['status']='Обнаружена ошибка. Повторите отправку еще раз';
 !empty($_POST['CITY']) ? $city=strip_tags($_POST['CITY']):$result['status']='Обнаружена ошибка. Повторите отправку еще раз';
 !empty($_POST['TYPE']) ? $type=strip_tags($_POST['TYPE']):$result['status']='Обнаружена ошибка. Повторите отправку еще раз';
-!empty($_POST['ENTITY']) ? $entity=strip_tags($_POST['ENTITY']):$result['status']='Обнаружена ошибка. Повторите отправку еще раз';
+// !empty($_POST['ENTITY']) ? $entity=strip_tags($_POST['ENTITY']):$result['status']='Обнаружена ошибка. Повторите отправку еще раз';
 
 // $mail=strip_tags($_POST['EMAIL']);
 // $mes=strip_tags($_POST['MESSAGE']);
@@ -19,7 +19,7 @@ if(empty($result['status'])){
     $headers  = "Content-type: text/html; charset=utf-8 \r\n";
     $subject = "Сообщение с сайта shop.abacuskids.ru";
     $headers .= "From: название сайта shop.abacuskids.ru\r\n";
-    $message = "ФИО: $lastname $name $fathersname <br> E-mail: $email <br> Телефон: $phone <br> Город: $city <br> Тип нозологии: $type <br> Тип заказчика: $entity";
+    $message = "ФИО: $lastname $name $fathersname <br> E-mail: $email <br> Телефон: $phone <br> Город: $city <br> Тип нозологии: $type ";
     mail($to, $subject, $message, $headers);
     
     $result['status']='success';
